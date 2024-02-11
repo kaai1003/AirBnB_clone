184930b9ecc42f15a372800087469b14f99a4bf2
hrmili <developerhamza.1@gmail.com>
2024-02-11 14:26:23 -0800

    Task 8

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Class user Module"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """class user definition"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
